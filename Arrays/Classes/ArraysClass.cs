d2ecccfaee73b561c165acf2fcca8a007eb70aa6
homeland <homeland@C6E3>
2023-02-23 17:25:09 +0300

    Добавьте файлы проекта.

---
+++ Arrays/Classes/ArraysClass.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Arrays.Classes
+{
+    public class ArraysClass
+    {
+        public int FindMinValue(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        public int FindMaxValue(int[] array)
+        {
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public int FindMaxIndexOValue(int[] array)
+        {
+            int maxIndex = 0;
+            int max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        public int FindMinIndexOValue(int[] array)
+        {
+            int minIndex = 0;
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        public int SumOfElementsWithOddIndexArray(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+
+            return sum;
+        }
+
+        public int[] ArrayReverse(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            int tmp = 0;
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                newArray[tmp] = array[i];
+                tmp++;
+            }
+            for (int j = 0; j < array.Length; j++)
+            {
+                array[j] = newArray[j];
+            }
+
+            return array;
+        }
+
+        public int SumOfOddElements(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum++;
+                }
+            }
+
+            return sum;
+        }
+
+        public int[] SwapPartsOfArray(int[] array)
+        {
+            int tmp = 0;
+            int[] newArray = new int[array.Length];
+            for (int i = array.Length / 2; i < array.Length; i++)
+            {
+                newArray[tmp] = array[i];
+                tmp++;
+            }
+            for (int j = 0; j < array.Length / 2; j++)
+            {
+                newArray[tmp] = array[j];
+                tmp++;
+            }
+            array = newArray;
+
+            return array;
+
+        }
+
+        public void PrintArr(int[] array)
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using Arrays.Classes;
+using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArraysClass arrays = new ArraysClass();
+            int min = arrays.FindMinValue(new int[] { 1, 23, 4, 56, 7 });
+
+            int sumOfIndex = arrays.SumOfElementsWithOddIndexArray(new int[] { 1, 23, 4, 56, 7 });
+            int[] arr = arrays.ArrayReverse(new int[] { 1, 23, 4, 56, 7 });
+            int sum = arrays.SumOfOddElements(new int[] { 1, 23, 4, 56, 7 });
+            int[] array = arrays.SwapPartsOfArray(new int[] { 1, 2, 3, 4, 5 });
+            arrays.PrintArr(array);
+
+            Console.ReadLine();
+        }
+    }
+}
