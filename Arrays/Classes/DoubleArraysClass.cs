a3b085417feb88c8b5d959e113c65fea31f15b77
xnesart <xnesart@mail.ru>
2023-03-03 01:32:19 +0300

    двумерные массивы

---
+++ Arrays/Classes/DoubleArraysClass.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Arrays.Classes
+{
+    internal class DoubleArraysClass
+    {
+        public int FindMinInDoubleArray(int[,] array)
+        {
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (min > array[i, j])
+                    {
+                        min = array[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        public int FindMaxInDoubleArray(int[,] array)
+        {
+            int max = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+        public string FindMaxIndexInDoubleArray(int[,] array)
+        {
+            int max = array[0, 0];
+            string index = "";
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (max < array[i, j])
+                    {
+                        max = array[i, j];
+                        index = ($"i = {i}, j = {j}");
+                    }
+                }
+            }
+            return index;
+        }
+        public string FindMinIndexInDoubleArray(int[,] array)
+        {
+            int min = array[0, 0];
+            string index = "";
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (min > array[i, j])
+                    {
+                        min = array[i, j];
+                        index = ($"i = {i}, j = {j}");
+                    }
+                }
+            }
+            return index;
+        }
+        //Найти количество элементов массива, которые больше своих левого и нижнего соседа одновременно.
+        public int FindCountOfElementsLargerThanTheirLeftAndBottomNeighbors(int[,] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if ((i == 0 || arr[i, j] > arr[i - 1, j])
+                    && (i >= arr.GetLength(0) - 1 || arr[i, j] > arr[i + 1, j])
+                    && (j == 0 || arr[i, j] > arr[i, j - 1])
+                    && (j >= arr.GetLength(1) - 1 || arr[i, j] > arr[i, j + 1]))
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        public void PrintDoubleArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine(array[i, j]);
+                }
+            }
+        }
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -8,14 +8,23 @@ namespace Arrays
         static void Main(string[] args)
         {
             ArraysClass arrays = new ArraysClass();
+            DoubleArraysClass doubleArr = new DoubleArraysClass();
             int min = arrays.FindMinValue(new int[] { 1, 23, 4, 56, 7 });
 
             int sumOfIndex = arrays.SumOfElementsWithOddIndexArray(new int[] { 1, 23, 4, 56, 7 });
             int[] arr = arrays.ArrayReverse(new int[] { 1, 23, 4, 56, 7 });
             int sum = arrays.SumOfOddElements(new int[] { 1, 23, 4, 56, 7 });
-           // int[] array = arrays.SortArrToMaxSelect(new int[] { 3,4,1,2 });
-            int[] array = arrays.SortArrToMinInsert(new int[] { 3,5,1,6,8,2 });
-            arrays.PrintArr(array);
+            // int[] array = arrays.SortArrToMaxSelect(new int[] { 3,4,1,2 });
+            int[] array = arrays.SortArrToMinInsert(new int[] { 3, 5, 1, 6, 8, 2 });
+            string index = doubleArr.FindMaxIndexInDoubleArray(new int[,]{ { 1, 2, 3 },
+                { 4,-4,-1} }
+            );
+            string indexx = doubleArr.FindMinIndexInDoubleArray(new int[,]{ { 1, 2, 3 },
+                { 4,-4,-1} });
+            int count = doubleArr.FindCountOfElementsLargerThanTheirLeftAndBottomNeighbors(new int[,]{ { 1, 2, 3 },
+                { 4,-4,-1} });
+            Console.WriteLine(count);
+            //doubleArr.PrintDoubleArray(minn);
 
             Console.ReadLine();
         }
